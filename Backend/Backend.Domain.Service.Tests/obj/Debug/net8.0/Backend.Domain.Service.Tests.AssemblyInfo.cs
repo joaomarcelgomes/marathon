702e387e89a626ab1f4ceb5054475954d81d349d
joaomarcelgomes <joaomarcelogomes7@gmail.com>
2024-03-14 15:00:59 -0300

    Adiciona git ignore

--- Backend/Backend.Domain.Service.Tests/obj/Debug/net8.0/Backend.Domain.Service.Tests.AssemblyInfo.cs
+++ Backend/Backend.Domain.Service.Tests/obj/Debug/net8.0/Backend.Domain.Service.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend.Domain.Service.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20102c4764e73e8dfd1a8b85a8d1da89f937c443")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend.Domain.Service.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend.Domain.Service.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Backend/Backend.Infra.EntityLibrary/obj/Debug/net8.0/Backend.Infra.EntityLibrary.AssemblyInfo.cs
+++ Backend/Backend.Infra.EntityLibrary/obj/Debug/net8.0/Backend.Infra.EntityLibrary.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend.Infra.EntityLibrary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f632e3a83bc05266c078754b57d373fdc0daf068")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend.Infra.EntityLibrary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend.Infra.EntityLibrary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
