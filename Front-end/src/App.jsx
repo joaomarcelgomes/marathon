469b4682fe96021876dec54c1de6db4be0485b6f
Wesley Barbosa <wesley.barbosa.developer@gmail.com>
2024-03-21 23:48:56 -0300

    Adiciona mapeamento de caminho

--- Front-end/src/App.jsx
+++ Front-end/src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css'
-import Register from './pages/Register/Register'
-import Login from './pages/Login/Login'
+import Register from '@/pages/Register/Register'
+import Login from '@/pages/Login/Login'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
